20953c78c626bd02e3d95571b5631136612882e9
Grigory <grigory@i.ru>
2025-05-06 22:41:08 +0300

    fullupd

--- mynumbers.py
+++ mynumbers.py
@@ -3,13 +3,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 def numbers():
     mynumbers = [random.randint(0, 10) for _ in range(5)]
     return mynumbers
 
+
 @app.route("/")
 def hello_world():
     return str(numbers())
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=80)
