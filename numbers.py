d11c3f418643b4c8917d721d1b07bf4478884621
GrigoryIll <121241597+GrigoryIll@users.noreply.github.com>
2025-05-06 22:26:39 +0300

    Update to flask numbers.py

--- numbers.py
+++ numbers.py
@@ -1,5 +1,15 @@
 import random
+from flask import Flask
+
+app = Flask(__name__)
 
 def numbers():
     mynumbers = [random.randint(0, 10) for _ in range(5)]
     return mynumbers
+
+@app.route("/")
+def hello_world():
+    return str(numbers())
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=80)
