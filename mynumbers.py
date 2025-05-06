b0be21e7037472e4a5725a01ddc4861c6315743c
GrigoryIll <121241597+GrigoryIll@users.noreply.github.com>
2025-05-07 01:58:20 +0300

    Update8 mynumbers.py

--- mynumbers.py
+++ mynumbers.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 def numbers():
-    mynumbers = [random.randint(0, 10) for _ in range(30)]
+    mynumbers = [random.randint(0, 10) for _ in range(15)]
     return mynumbers
 
 
